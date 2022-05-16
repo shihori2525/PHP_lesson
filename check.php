e73b5c8ff45fec8a6ef2e104362251340d179960
shihori2525 <rirakkuma.250@gmail.com>
2022-05-16 22:35:44 +0900

    [Add]validation

--- check.php
+++ check.php
@@ -10,12 +10,32 @@
 
   <?php
   $nickname = $_POST['nickname'];
+  $email = $_POST['email'];
+  $goiken = $_POST['goiken'];
+
   if ($nickname == '') {
-    print 'ニックネームが入力されていません。';
+    print 'ニックネームが入力されていません。<br/>';
   } else {
     print 'ようこそ';
     print $nickname;
     print '様';
+    print '<br/>';
+  }
+
+  if ($email == '') {
+    print 'メールアドレスが登録されていません。<br/>';
+  } else {
+    print 'メールアドレス';
+    print $email;
+    print '<br/>';
+  }
+
+  if ($goiken == '') {
+    print 'ご意見が入力されていません。<br/>';
+  } else {
+    print 'ご意見<br/>';
+    print $goiken;
+    print '<br>';
   }
   ?>
 </body>
