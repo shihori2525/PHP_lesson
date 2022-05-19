24da97b84a9b662ca13b4d54963dad54adb3513d
shihori2525 <rirakkuma.250@gmail.com>
2022-05-19 22:11:38 +0900

    [Add]thanks page

--- thanks.php
+++ thanks.php
@@ -8,7 +8,18 @@
 
 <body>
   <?php
+  $nickname = $_POST['nickname'];
+  $email = $_POST['email'];
+  $goiken = $_POST['goiken'];
+
+  print $nickname;
+  print '様<br/>';
   print 'ご意見ありがとうございました。<br/>';
+  print 'いただいたご意見『';
+  print $goiken;
+  print '』<br/>';
+  print $email;
+  print 'にメールを送りましたのでご確認ください。';
   ?>
 </body>
 
