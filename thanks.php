21d6f659d5c8128b80a9c5144f92de164fe4073f
shihori2525 <rirakkuma.250@gmail.com>
2022-05-19 22:43:20 +0900

    [Add]thanks page

--- thanks.php
+++ thanks.php
@@ -20,6 +20,14 @@
   print '』<br/>';
   print $email;
   print 'にメールを送りましたのでご確認ください。';
+
+  $mail_sub = 'アンケート受け付けました。';
+  $mail_body = $nickname . "様へ\nアンケートご協力ありがとうございました。";
+  $mail_body = html_entity_decode($mail_body, ENT_QUOTES, "UTF-8");
+  $mail_head = 'From:xxx@xxx.co.jp';
+  mb_language('Japanese');
+  mb_internal_encoding("UTF-8");
+  mb_send_mail($email, $mail_sub, $mail_body, $mail_head);
   ?>
 </body>
 
