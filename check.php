0d6707691b2f7de10192cabc9a16aef0ddd0d7ed
shihori2525 <rirakkuma.250@gmail.com>
2022-05-19 22:24:48 +0900

    [Add]Sanitizing

--- check.php
+++ check.php
@@ -13,6 +13,10 @@
   $email = $_POST['email'];
   $goiken = $_POST['goiken'];
 
+  $nickname = htmlspecialchars($nickname);
+  $email = htmlspecialchars($email);
+  $goiken = htmlspecialchars($goiken);
+
   if ($nickname == '') {
     print 'ニックネームが入力されていません。<br/>';
   } else {
